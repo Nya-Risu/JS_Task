3a569a4e0d5e6bf19ca24de9dc3b9b907544b2df
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:10:35 +0300

    js_task

---
+++ 67/js/script67.js
@@ -0,0 +1,16 @@
+let test = {x: 1, y: 2, z: 3};
+console.log(test);
+// 2
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1.x);
+// 3
+let test2 = [1, 2, 3];
+console.log(test2);
+// 4
+let test3 = [1, 2, 3];
+console.log(test3[1]);
+// 5
+let test5 = [1, 2, 3];
+let test4 = 1;
+
+console.log(test5);
