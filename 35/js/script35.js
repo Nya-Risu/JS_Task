c46f2a3c9ee13df2dbba144fa73042a73d6611ea
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:00:23 +0300

    js_task

---
+++ 35/js/script35.js
@@ -0,0 +1,41 @@
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ', num1 + num2); 
+// 2
+let a = 1;
+let b = 2;
+console.log(a + b);
+// 3
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+// 4
+let num3 = '123';
+console.log(num3[0]);
+// 5
+let a1 = 0;
+console.log(++a1);
+// 6
+let num4 = '123';
+console.log(num4.length);
+// 7
+let a2 = 24 / 60 / 60;
+console.log(a2.toFixed(4));
+// 8
+let num5 = 123;
+let str = String(num5);
+console.log(str.length);
+// 9
+let num6 = 123;
+let str1 = String(num6);
+console.log(str1[str1.length-1]);
+// 10
+let a3 = '123';
+let b3 = '456';
+let s = Number(a3) + String(b3);
+console.log(Number(s));
+
+
+
+
+
