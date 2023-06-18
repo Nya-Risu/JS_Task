361813d0d49169d54d1549f00621089beb15e7c8
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:58:17 +0300

    js_task

---
+++ 26/js/script26.js
@@ -0,0 +1,20 @@
+let one = Boolean(3);
+console.log(one);
+// 2
+let two = Boolean(0);
+console.log(two);
+// 3
+let three = Boolean(-1);
+console.log(three);
+// 4
+let four = Boolean(-0);
+console.log(four);
+// 5
+let five = Boolean('alec');
+console.log(five);
+// 6
+let six = Boolean(1/0);
+console.log(six);
+// 7 
+let one1 = Boolean(3 * 'abs');
+console.log(one1);
