98c8d101f050f4ec09782d4d71515843db15639f
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:14:41 +0300

    js_task

---
+++ 81/js/script81.js
@@ -0,0 +1,21 @@
+let num = 3;
+if (num > 0 && num < 5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 2
+let num1 = 11;
+if (num1 >= 10 && num1 <=20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 3
+let num3 = -1;
+let num2 = 4;
+if (num2 >= 3 && num3 <=1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
