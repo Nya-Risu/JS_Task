3cc5ac240d1d9bf7cbbd9bc3a38723be27f5aedb
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:57:51 +0300

    js_task

---
+++ 24/js/script24.js
@@ -0,0 +1,8 @@
+let str = '123', str1 = '456';
+console.log(String(str)+String(str1));
+// 2
+let str4 = '123';
+console.log(String(str4).length);
+// 3
+let str2 = '123', str3 = '456';
+console.log(String(str2).length+String(str3).length);
