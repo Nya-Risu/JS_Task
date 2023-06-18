52840a455dee6d57af2ab9020f3444d2e0b9fc12
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:48:27 +0300

    js_task

---
+++ 6/js/script6.js
@@ -0,0 +1,22 @@
+let hunter = 632;
+alert(hunter);
+// number 2
+let a = 2;
+let b = 4;
+let c = 6;
+
+let d = 2, f = 4, e = 6;
+
+let aa, bb, cc;
+aa = 2;
+bb = 4;
+cc = 6;
+// number 3
+let hunt;  
+
+hunt = 10;    
+alert(hunt); 
+
+hunt = 20;     
+alert(hunt); 
+
