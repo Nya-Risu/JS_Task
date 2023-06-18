a3690ca71592ed90ea30c78fc7d2bde0279fdf97
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:00:46 +0300

    js_task

---
+++ 37/js/script37.js
@@ -0,0 +1,20 @@
+let r = 4, pi = 3.14;
+console.log(pi * r ** 2);
+// 2
+let a = 19;
+s = a ** 2;
+console.log(s);
+// 3
+let b = 13, c = 15;
+s = b + c;
+console.log(s);
+// 4
+let d = 13, f = 15;
+s = d * f;
+console.log(s);
+// 5
+let tc = 14;
+console.log(tc * 33.8);
+// 6
+let tf = 14;
+console.log((tf - 32) / 1.8);
