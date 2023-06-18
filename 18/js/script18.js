eb458b22aa8e3119f110c73c285e26ed3c1a0c9e
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:55:22 +0300

    js_task

---
+++ 18/js/script18.js
@@ -0,0 +1,18 @@
+let a;
+alert(a);
+// number 2
+let b = null;
+alert(b);
+// number 3
+let c = true;
+alert(c);
+// number 4
+let d = false;
+alert(d);
+// number 5
+let e = 'Alec', f = 'Lightwood';
+alert(e * f);
+// number 6
+alert( 10 / 0);
+// number 7 
+alert(-10 / 0); 
