e1cd8ee12df129dd5e14f8483c0e029edbec3efe
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:17:13 +0300

    js_task

---
+++ 89/js/script89.js
@@ -0,0 +1,13 @@
+let test = true;
+if (!test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 2
+let test1 = true;
+if (!test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
