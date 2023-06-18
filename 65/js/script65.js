ff18d993a5070f95fe29aaf839458737d8189572
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:10:14 +0300

    js_task

---
+++ 65/js/script65.js
@@ -0,0 +1,12 @@
+console.log( typeof {x: 1, y: 2, z: 3} ); 
+// 2
+console.log( typeof [1, 2, 3] );
+// 3
+let arr = [1, 2, 3];
+console.log( typeof arr );
+// 4
+let arr1 = [1, 2, 3];
+console.log( typeof arr[0] );
+// 5
+let arr2 = ['1', '2', '3'];
+console.log( typeof arr[0] );
