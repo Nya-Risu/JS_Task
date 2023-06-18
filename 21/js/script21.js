dd649fb672321a9f675757265a5a3a56c5e95ec1
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:57:05 +0300

    js_task

---
+++ 21/js/script21.js
@@ -0,0 +1,23 @@
+let a = '5' + '2';
+alert(a);
+// 2
+let b = '5' + 2;
+alert(b);
+// 3
+let c = '5' * '2';
+alert(c);
+// 4
+let d = '5' - '2';
+alert(d);
+// 5
+let e = '5' / '2';
+alert(e);
+// 6
+let f = '5' % '2';
+alert(f);
+// 7
+let g = '5s' * '2';
+alert(g);
+// 8
+let k = '' + 3 + 1;
+alert(k);
