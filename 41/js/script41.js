a3fd41c8d3f06d926c8f6cfd2359fda35750d734
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:01:43 +0300

    js_task

---
+++ 41/js/script41.js
@@ -0,0 +1,17 @@
+let arr = ['a', 'b', 'c'];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+// 2
+let arr1 = [1, 2, 3, 4];
+arr1[0] += 3;
+arr1[1] += 3;
+arr1[2] += 3;
+arr1[3] += 3;
+console.log(arr1)
+// 3
+let arr2 = [1, 2, 3];
+arr2[0]++;
+arr2[1]++;
+arr2[2]++;
+console.log(arr2);
