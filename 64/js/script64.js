37f2597b630b11660dde45efa671e55a3c59eb1a
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:10:04 +0300

    js_task

---
+++ 64/js/script64.js
@@ -0,0 +1,10 @@
+console.log( typeof {x: 1, y: 
+	2, z: 3} ); 
+// 2
+console.log( typeof {} );
+// 3
+let obj = {x: 1, y: 2, z: 3};
+console.log( typeof obj );
+// 4
+let obj1 = {x: 1, y: 2, z: 3};
+console.log( typeof obj1['x'] );
