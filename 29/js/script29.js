6efafa4eb93a12e753b04711f551e4c594f4f188
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:58:51 +0300

    js_task

---
+++ 29/js/script29.js
@@ -0,0 +1,11 @@
+let hunter = '12345'; 
+console.log(Number(hunter[0]) + Number(hunter[1])+Number(hunter[2]) + Number(hunter[3])+Number(hunter[4]))
+// 2
+let hunt = String(12345); 
+console.log(Number(hunt[0]) + Number(hunt[1])+ Number(hunt[2]) + Number(hunt[3])+ Number(hunt[4]))
+// 3
+let hunt1 = String(12345); 
+console.log(hunt1[0] * hunt1[1] * hunt1[2] * hunt1[3]* hunt1[4]);
+// 4
+let hunt2 = '12345'; 
+console.log(hunt2[4] + hunt2[3] + hunt2[2] + hunt2[1] + hunt2[0]);
