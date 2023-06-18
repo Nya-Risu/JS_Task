94f3b331bc41265bd21f519d6ee1c3117dc26897
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:57:37 +0300

    js_task

---
+++ 23/js/script23.js
@@ -0,0 +1,8 @@
+let a = parseInt('5px'), b = parseInt('6px');
+console.log(a+b);
+// 2
+let c = parseFloat('5.5px'), d = parseFloat('6.25px');
+console.log(c+d);
+// 3
+let c1 = parseFloat('5.5px'), d1 = parseFloat('6.25px');
+console.log(c1+d1+'px');
