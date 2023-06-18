66871482a98fc46e795e8e18a45c5ef1b50311a4
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:15:55 +0300

    js_task

---
+++ 86/js/script86.js
@@ -0,0 +1,13 @@
+let test = true;
+if (test === true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 2
+let test1 = true;
+if (test1 === false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
