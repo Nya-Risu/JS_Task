09d11d2bfd0f3a110ce95ebe6f5d47aa496f0ced
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:06:46 +0300

    js_task

---
+++ 47/js/script47.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length-1]);
+// 2
+
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1[0] + arr1[1] + arr1[2] + arr1[3] + arr1[4]); 
+// 3
+
+let arr2 = [1, 2, 3, 4, 5];
+console.log(arr2.length);
