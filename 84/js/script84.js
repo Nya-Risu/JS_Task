b241889f6677f03972ceaaa03f205c35efa217c8
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:15:11 +0300

    js_task

---
+++ 84/js/script84.js
@@ -0,0 +1,23 @@
+let num = 3;
+if ( (num > 5 && num < 10) || (num 
+	== 20)) { 
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 2
+let num1 = 3;
+		
+if (num1 > 5 || (num1 > 0 && num1 < 3)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 3
+let num2 = 3;
+if (num2 == 9 || (num2 > 10 && num2 
+	< 20) || (num2 > 20 && num2 < 30)) { 
+	console.log('+++');
+} else {
+	console.log('---');
+}
