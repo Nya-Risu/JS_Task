9f6e73d09fb3e7a840454ea0ea79b343330fc485
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:00:11 +0300

    js_task

---
+++ 34/js/script34.js
@@ -0,0 +1,14 @@
+document.write('Alec');
+// 2
+document.write('<p style="font-style: italic;">Alec</p>');
+// 3
+let str = 'text';
+document.write('<p style="font-style: italic;">str</p>');
+// 4
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
+
+
