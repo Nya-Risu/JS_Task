c21e4b2bb97e853cba28a5cf13e56bc4732e6d88
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:49:05 +0300

    js_task

---
+++ 8/js/script8.js
@@ -0,0 +1,18 @@
+let a = 10, b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+// number 2
+let a1 = 10, b1 = 5;
+result = a1 + b1;
+alert(result);
+// number 3
+let a2 = 1, b2 = 2, c2 = 3;
+alert(a2 + b2 + c2);
+// number 4
+let a3 = 10, b3 = 5;
+c = a3 - b3;
+let d = 7;
+result1 = c + d;
+alert(result1);
