cf32ffd87f6d27308468b1ec4d07084a087ca64d
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 02:14:50 +0300

    js_task

---
+++ 82/js/script82.js
@@ -0,0 +1,22 @@
+let num1 = -10;
+let num2 = -10;
+if (num1 >= 0 || num2 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+} // ---
+// 2
+let num3 = 0;
+let num4 = 0;
+if (num3 >= 0 || num4 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+} //+++
+// 3
+let num = 1;
+if (num == 0 || num == 1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
