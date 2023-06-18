d07495027965d404daca5929c32210aa3c37d272
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 01:58:29 +0300

    js_task

---
+++ 27/js/script27.js
@@ -0,0 +1,11 @@
+let str = 'abcde';
+console.log(str[0]); 
+console.log(str[2]); 
+console.log(str[4]); 
+// 2
+let str1 = 'abcde';
+console.log(str1[4]+str1[3]+str1[2]+str1[1]+str1[0]); 
+// 3
+let str2 = 'abcde';
+let num = 3; 
+console.log(str2[num]); 
