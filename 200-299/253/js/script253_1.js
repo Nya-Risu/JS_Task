6e59cafbc45498e5a4a8fbe025392a667b95da99
Nya-Risu <grivko.v.i.21@gmail.com>
2023-06-19 03:48:50 +0300

    js_task

---
+++ 200-299/253/js/script253_1.js
@@ -0,0 +1,6 @@
+document.querySelector('#image').src = 'favicon.ico';
+// 2
+let img2 = document.querySelector('#image2');
+    img2.src = 'favicon.ico'
+    img2.height = '100'
+    img2.width = '200'
